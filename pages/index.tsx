658bfa3d96851cc4376ec87532f1cf06469912a9
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-05-24 15:10:58 -0600

    Configuracion del shoplayout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
+import { ShopLayout } from "@/components/layouts";
+import { Typography } from "@mui/material";
 
 
 export default function Home() {
   return (
-    <h1>Home Page</h1>
+    <ShopLayout title="Teslo-Shop - Home" pageDescription="Encuentra los mejores productos de Teslo aqui">
+      <Typography variant="h1" component='h1'>Tienda</Typography>
+      <Typography variant="h2" sx={{mb: 1}}>Todos los productos</Typography>
+
+    </ShopLayout>
   )
 }
