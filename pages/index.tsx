133c81ef104b3fadce90a6ef4c95e03f5c209399
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-05-25 09:34:18 -0600

    Trabajando en pagina principal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
+import { CardActionArea, CardMedia, Grid, Typography } from "@mui/material";
 import { ShopLayout } from "@/components/layouts";
-import { Typography } from "@mui/material";
+import { initialData } from "@/database/products";
+
+
 
 
 export default function Home() {
@@ -7,6 +10,25 @@ export default function Home() {
     <ShopLayout title="Teslo-Shop - Home" pageDescription="Encuentra los mejores productos de Teslo aqui">
       <Typography variant="h1" component='h1'>Tienda</Typography>
       <Typography variant="h2" sx={{mb: 1}}>Todos los productos</Typography>
+      
+
+
+      <Grid container spacing={4}>
+        {
+          initialData.products.map( product => (
+            <Grid item xs={ 5 } sm ={ 4 } key={ product.slug }>
+              <CardActionArea>
+                <CardMedia
+                  component='img'
+                  image={ `products/${product.images[0]}` }
+                  alt={product.title}
+                />
+              </CardActionArea>
+            </Grid>
+          ))
+        }
+      </Grid>
+
 
     </ShopLayout>
   )
