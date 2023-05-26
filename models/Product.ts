17a529aebd9ba6188e942530d1b0203732fc0bb6
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-05-26 12:09:39 -0600

    Definicion del modelo

---
+++ models/Product.ts
@@ -0,0 +1,40 @@
+import { IProduct } from '@/interfaces'
+import mongoose,{ Schema, model, Model} from 'mongoose'
+
+const productSchema = new Schema({
+  description: { type: String, require: true},
+  images: [{ type: String }],
+  inStock: { type: Number, require: true, default: 0},
+  price: { type: Number, require: true, default: 0},
+  sizes: [{
+    type: String,
+    enum: {
+      values: ['XS','S','M','L','XL','XXL','XXXL'],
+      message: '{VALUE} no es un tamaño valido'
+    }
+  }],
+  slug: { type: String, require: true, unique: true},
+  tags: [ { type: String } ],
+  title: { type: String, require: true },
+  type: {
+    type: String,
+    enum: {
+      values: ['shirts','pants','hoodies','hats'],
+      message: '{VALUE} no es un tipo valido'
+    }
+  },
+  gender: {
+    type: String,
+    enum: {
+      values: ['men','women','kid','unisex'],
+      message: '{VALUE} no es un genero valido'
+    }
+  }
+}, { 
+  timestamps: true 
+
+})
+
+const Product: Model<IProduct> = mongoose.models.Product || model('Product', productSchema)
+
+export default Product;
