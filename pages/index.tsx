834a860632be4b5984d90cfbe0847960ed482692
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-05-27 20:13:32 -0600

    Implementando swr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,24 @@
 import { CardActionArea, CardMedia, Grid, Typography } from "@mui/material";
 import { ShopLayout } from "@/components/layouts";
-import { initialData } from "@/database/products";
 import { ProductList } from "@/components/products";
 
+import useSWR from 'swr'
+const fetcher = (...args: [ key: string ]) => fetch(...args).then(res => res.json())
 
 export default function Home() {
+
+  const { data, error, isLoading } = useSWR('/api/products', fetcher)
+
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+
   return (
     <ShopLayout title="Teslo-Shop - Home" pageDescription="Encuentra los mejores productos de Teslo aqui">
       <Typography variant="h1" component='h1'>Tienda</Typography>
       <Typography variant="h2" sx={{mb: 1}}>Todos los productos</Typography>
       
       <ProductList 
-        products={ initialData.products as any }      
+        products={ data as any }      
       />
 
     </ShopLayout>
