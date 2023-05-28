d59d8ef795daa62dc01c089845500d143f6236a6
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-05-27 20:27:55 -0600

    Configurando swr de forma global

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,26 @@
-import { CardActionArea, CardMedia, Grid, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import { ShopLayout } from "@/components/layouts";
 import { ProductList } from "@/components/products";
+import { useProducts } from "@/hooks";
 
-import useSWR from 'swr'
-const fetcher = (...args: [ key: string ]) => fetch(...args).then(res => res.json())
 
 export default function Home() {
 
-  const { data, error, isLoading } = useSWR('/api/products', fetcher)
-
-  if (error) return <div>failed to load</div>
-  if (isLoading) return <div>loading...</div>
-
+  const { products, isLoading } = useProducts('/products')
+  
   return (
     <ShopLayout title="Teslo-Shop - Home" pageDescription="Encuentra los mejores productos de Teslo aqui">
       <Typography variant="h1" component='h1'>Tienda</Typography>
       <Typography variant="h2" sx={{mb: 1}}>Todos los productos</Typography>
       
-      <ProductList 
-        products={ data as any }      
-      />
+      {
+        isLoading
+        ? <h1>Cargando...</h1>
+        : <ProductList 
+            products={ products }      
+          />
+        
+      }
 
     </ShopLayout>
   )
