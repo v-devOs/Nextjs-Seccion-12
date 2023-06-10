879133c332b837082d18e5c3b0a08b2257ff13b8
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-06-10 11:19:18 -0600

    Creando modelo de los usuarios

---
+++ interfaces/IUser.ts
@@ -0,0 +1,12 @@
+
+export interface IUser{
+  _id      : string
+  name     : string
+  email    : string
+  password?: string
+  roel     : string
+
+
+  createdAt : string
+  updatedAt: string
+}
---
+++ models/User.ts
@@ -0,0 +1,27 @@
+import { IUser } from '@/interfaces';
+import mongoose, { Schema, model, Model } from 'mongoose'
+
+
+const userSchema = new Schema({
+
+  name    :{ type: String, require: true },
+  email   : { type: String, require: true, unique: true},
+  password:{ type: String, require: true },
+
+  roel: {
+    type: String,
+    enum: {
+      values: ['admin', 'client'],
+      message: '{VALUE} no es un role valido',
+      default: 'client',
+      required: true
+    }
+  }
+},{
+  timestamps: true,
+})
+
+
+const User: Model<IUser> = mongoose.models.User || model('User', userSchema);
+
+export default User;
