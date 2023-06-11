02686d8ca90cb1d9684dc47dad591a6940ee422e
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-06-11 12:49:32 -0600

    Registrando nuevos usuarios, falta validar correos

---
+++ pages/api/user/register.ts
@@ -0,0 +1,81 @@
+import { db } from '@/database'
+import User from '@/models/User'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import bcryptjs from 'bcryptjs';
+import { jwt } from '@/utils';
+
+type Data = { message: string }
+| { 
+  token: string,
+    user: {
+      email: string, 
+      role: string, 
+      name: string
+    }
+}
+
+export default function handler (req: NextApiRequest, res: NextApiResponse<Data>) {
+  
+  switch( req.method ){
+
+    case 'POST':
+      return registerUser( req, res )
+
+    default:
+      res.status(400).json({
+        message: 'Bad Request'
+      })
+
+  }
+}
+
+const registerUser = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
+  
+  const { email = '', password = '', name = '' } = req.body as { email: string, password: string, name: string}
+
+  await db.connect()
+
+  const user = await User.findOne({ email })
+
+  if( password.length < 6 ){
+    return res.status(400).json({message: 'La contraseña debe de ser 6 caracteres'})
+  }
+
+  if( name.length < 2 ){
+    return res.status(400).json({message: 'El nombre debe de ser 3 caracteres'})
+  }
+
+  if( user ){
+    await db.disconnect()
+    return res.status(400).json({ message: 'Correo ya esta registrado'})
+  }
+
+  const newUser = new User({
+    email: email.toLocaleLowerCase(),
+    password: bcryptjs.hashSync( password ),
+    role: 'client',
+    name    
+  });
+
+  try {
+
+    await newUser.save({ validateBeforeSave: true })
+    
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ message: 'Revisar logs del servirdor'})
+  }
+
+  const { _id } = newUser;
+
+  const token = jwt.signToken( _id, email)
+
+  return res.status(200).json({
+    token,
+    user: {
+      email, 
+      role: 'client', 
+      name
+    }
+  })
+}
--- utils/jwt.ts
+++ utils/jwt.ts
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 
 export const signToken = (_id: string, email: string ) => {
-  if( !process.env.JWT_SECRET_SEED){
+  if( !process.env.JWT_SECRET_SEED ){
     throw new Error('No hay emilla de JWT')
   }
 
