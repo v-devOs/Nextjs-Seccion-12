d600d855e2ffffd450b8076917e7b6dbf69cea7c
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-06-22 17:18:58 -0600

    NextAuth callbacks

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,14 +2,13 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import Credentials from "next-auth/providers/credentials"
+import { IUser } from "@/interfaces"
 
-export const authOptions = {
+export default NextAuth({
+  
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
+    
     // ...add more providers here
 
     Credentials({
@@ -24,9 +23,15 @@ export const authOptions = {
 
         // TODO: VALIDAR CONTRA BASE DE DATOS
 
-        return null
+        return { name: 'Uriel', role: 'admin', email: 'uriel@google.com', id: ''}
       }
     }),
+
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+
 
   ],
 
@@ -34,10 +39,42 @@ export const authOptions = {
 
   },
 
-  // Callbacks
-  callbacks: {
-    
-  }
-}
+  
 
-export default NextAuth(authOptions)
+  // Callbacks
+
+  callbacks: {
+    async jwt({token, user, account}){
+      // console.log({ token, user, account }, 'Impresion del callback jwt')
+
+      if( account ){
+
+        token.accessToken = account.access_token
+
+        switch( account.type ){
+
+          case 'oauth':
+            // TODO: crear usuario o verificar si existe en base de datos
+            break;
+          case 'credentials':
+            token.user = user
+
+            break;
+        }
+      }
+
+      return token
+    },
+
+    async session({ session, token, user }: any){
+
+      session.accessToken = token.accessToken
+      session.user = token.user as any
+
+      return session
+    }
+  },
+
+  
+})
+
