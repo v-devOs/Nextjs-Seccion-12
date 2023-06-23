52465b440ccf2c05e3ae93b367cc9e198014a4ab
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-06-23 14:08:34 -0600

    Middleware con nextauth

--- pages/checkout/address.tsx
+++ pages/checkout/address.tsx
@@ -208,31 +208,4 @@ const AddressPage = () => {
   )
 }
 
-
-// You should use getServerSideProps when:
-// - Only if you need to pre-render a page whose data must be fetched at request time
-
-export const getServerSideProps: GetServerSideProps = async ({req}) => {
-
-  const { token = '' } = req.cookies;
-
-  const isValidToken = await validateSession( token )
-
-  if( !isValidToken ){
-    return {
-      redirect: {
-        destination: '/auth/login?p=/checkout/address',
-        permanent: false
-      }
-    }
-  }
-
-  return {
-    props: {
-      
-    }
-  }
-}
-
-
 export default AddressPage
