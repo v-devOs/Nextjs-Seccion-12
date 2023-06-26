c1b5dc214491855dad912d380cebe7eccfc06bc3
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-06-26 15:24:22 -0600

    Creando el modelo de mongo usando la IOrder interface

--- models/Order.ts
+++ models/Order.ts
@@ -4,9 +4,36 @@ import { IOrder } from '@/interfaces';
 
 const orderSchema = new Schema({
 
-  name    :{ type: String, require: true },
-  
-  
+  user: { type: Schema.Types.ObjectId, ref: 'User', require: true },
+  orderItems: [{
+    _id:        { type:Schema.Types.ObjectId, ref: 'Product', require: true},
+    title:      { type: String, require: true},
+    size:       { type: String, require: true},
+    quantity:   { type: Number, require: true},
+    slug:       { type: String, require: true},
+    image:      { type: String, require: true},
+    prize:      { type: Number, require: true},
+  }],
+
+  shippingAddress: {
+    firstName: { type: String, require: true},
+    lastName:  { type: String, require: true},
+    address:   { type: String, require: true},
+    address2:  { type: String },
+    zip:       { type: String, require: true},
+    city:      { type: String, require: true},
+    country:   { type: String, require: true},
+    phone:     { type: String, require: true},
+  },
+
+  numberOfItems: { type: Number, require: true},
+  subtotal:      { type: Number, require: true},
+  tax:           { type: Number, require: true},
+  total:         { type: Number, require: true},
+
+  isPaid: { type: Boolean, require: true, default: false},
+  paidAt: { type: String },
+
 },{
   timestamps: true,
 })
