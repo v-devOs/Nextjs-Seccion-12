16fdfa4285e2bcdf97f8cda28df399e76c846198
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-06-27 15:12:55 -0600

    Fin de la validacion de la orden del lado del servidor

--- models/index.ts
+++ models/index.ts
@@ -1,2 +1,3 @@
 export { default as Product } from './Product'
 export { default as USer } from './User'
+export { default as Order } from './Order'
--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -1,11 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { IOrder } from '../../../interfaces/IOrder';
-import { getSession } from 'next-auth/react';
 import { db } from '@/database';
-import { Product } from '@/models';
+import { Product, Order } from '@/models';
 import { getToken } from 'next-auth/jwt';
 
-type Data = { message: string }
+type Data = { message: string } | IOrder
 
 export default function handler (req: NextApiRequest, res: NextApiResponse<Data>) {
   
@@ -42,16 +41,38 @@ const createOrder = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
   try {
     const subTotal = orderItems.reduce( (prev, current) => {
 
-      const currentPrice = dbProducts.find( prod => prod._id === current._id)?.price
+      const currentPrice = dbProducts.find( prod => prod.id === current._id)?.price
       
       if( !currentPrice ){
         throw new Error('Verifique el carrito de nuevo, producto no existente')
       }
-
       return ( currentPrice * current.quantity) + prev
-    } ,0)
-  } catch (error) {
-    
+    },0)
+
+    const taxRate = Number( process.env.NEXT_PUBLIC_TAX_RATE || 0 )
+    const backendTotal = ( subTotal * taxRate ) +  subTotal
+
+    if( total !== backendTotal ){
+      throw new Error('El total no cuadra con el monto')
+    }
+
+    // Todo bien hasta aqui
+    const userId = session.user._id
+    const newOrder = new Order({...req.body, isPaid: false, user: userId})
+
+    await newOrder.save()
+
+    await db.disconnect()
+
+    return res.status(200).json(newOrder)
+
+  } catch (error: any) {
+    await db.disconnect()
+    console.log(error)
+
+    res.status(400).json({
+      message: error.message || 'Revise los logs del servidor'
+    })
   }
   
 
